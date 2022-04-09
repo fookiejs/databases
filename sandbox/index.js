897161964d785225b386b74b843a31ef7a49fe77
umudik <seyitumutbicer@gmail.com>
2022-04-09 03:59:29 +0300

    dev

--- sandbox/index.js
+++ sandbox/index.js
@@ -3,19 +3,27 @@
     const fookie = require("../../core")
     await fookie.init()
     /*
-    await fookie.setting({
-        name: "postgresql_connection",
-        value: {
-           url:`postgres://guest:guest@127.0.0.1/fookie`
-        }
-    })
+        await fookie.setting({
+            name: "postgresql_connection",
+            value: {
+                url: `postgres://guest:guest@127.0.0.1:5432/fookie`
+            }
+        })
+        await fookie.use(require("../").postgresql)
     */
+
     await fookie.setting({
         name: "mongodb_connection",
         value: { url: "mongodb://127.0.0.1/fookie" }
     })
-    // await fookie.use(require("../").postgresql)
     await fookie.use(require("../").mongodb)
+
+
+
+
+
+
+
     await fookie.use(require("../../server"))
     await fookie.listen(3002)
 
